eea0f29766ccc8e64df445c0c168c10c6b8023a5
Damien Carcel <damien.carcel@akeneo.com>
2019-05-15 14:51:43 +0200

    AOB-331: Rename the project as "Patoche"

--- tests/spec/Application/Vcs/GetNextTagHandlerSpec.php
+++ tests/spec/Application/Vcs/GetNextTagHandlerSpec.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Patrick Tag.
+ * This file is part of Patoche.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
