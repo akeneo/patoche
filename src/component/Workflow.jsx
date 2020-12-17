0842de9684d1bcea7b050f890968641c04581cc9
Aurélie MAILLOT <aureliemaillot07@gmail.com>
2020-12-17 10:19:21 +0100

    Issue 36:Display the avatar of person who triggered the build

--- src/component/Workflow.jsx
+++ src/component/Workflow.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Workflow.css';
 
 const Workflow = (props) => {
   const url = `https://app.circleci.com/pipelines/github/akeneo/onboarder/${props.workflow.pipelineNumber}/workflows/${props.workflow.id}`;
 
   return (
     <span>
+      <img src={props.workflow.triggeredBy.avatar_url} alt={props.workflow.triggeredBy.login} />
       <a href={url}>{props.workflow.id}</a>
     </span>
   );
 };
 
 Workflow.propTypes = {
-  workflow: PropTypes.shape({ id: PropTypes.string, pipelineNumber: PropTypes.number }),
+  workflow: PropTypes.shape({
+    id: PropTypes.string,
+    pipelineNumber: PropTypes.number,
+    triggeredBy: PropTypes.shape({ login: PropTypes.string, avatar_url: PropTypes.string }),
+  }),
 };
 
 export default Workflow;
