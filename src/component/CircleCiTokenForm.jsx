4af947120ed91a37d238a36bdfdede1d30f9be8c
Aurélie MAILLOT <aureliemaillot07@gmail.com>
2020-12-18 09:51:47 +0100

    Issue 43: Document and format how to generate a token

--- src/component/CircleCiTokenForm.jsx
+++ src/component/CircleCiTokenForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
+import './CircleCiTokenForm.css';
 
 const CircleCiTokenForm = (props) => {
   const { circleToken: setCircleToken } = {
@@ -14,12 +15,22 @@ const CircleCiTokenForm = (props) => {
     setCircleToken(data.token);
   };
 
+  const url = `https://app.circleci.com/settings/user/tokens`;
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input name="token" type="text" ref={register({ required: true })} />
-      {errors.exampleRequired && <span>This field is required</span>}
-      <input type="submit" value="Save" />
-    </form>
+    <div className="circle-ci-token-form">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input className="token" name="token" type="text" ref={register({ required: true })} />
+        {errors.exampleRequired && <span>This field is required</span>}
+        <input className="submit" type="submit" value="Save" />
+      </form>
+      <p>Please enter your CircleCI token to access the application.</p>
+      <p>
+        Please follow <a href={url}>these instructions</a> to generate your personal CircleCI token and copy it here.
+      </p>
+      <p className="warning">Warning!</p>
+      <p>You can only use this application if you have access to the Onboader repository on Github.</p>
+    </div>
   );
 };
 
